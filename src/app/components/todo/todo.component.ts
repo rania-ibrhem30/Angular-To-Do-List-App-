83b6dc0f74a9a22d224cbd52c1bac9e5c5d0849f
rania-ibrhem30 <raniamansour967@gmail.com>
2024-05-22 23:32:03 +0300

    edit save in local storage

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -15,29 +15,59 @@ export class TodoComponent {
   tasks: string[] = [];
   isAvalible: boolean = false;
 
+  // addTask() {
+  //   if (this.newTask.trim() !== "") {
+  //     this.tasks.push(this.newTask)
+  //     this.isAvalible = true;
+  //     this.newTask = ""
+  //     console.log(this.tasks)
+  //   }
+  // }
+  // RemoveTask(index: number) {
+  //   this.tasks.splice(index, 1);
+  //   this.isAvalible = this.tasks.length > 0;
+
+  // }
+  // EditTask(index: number, editnewtask: string): string | void {
+  //   if (editnewtask.trim() !== "") {
+  //     this.tasks[index] = editnewtask;
+
+  //   }
+  //   else {
+  //     editnewtask = this.tasks[index];
+  //     return this.newTask = editnewtask
+  //   }
+  //   this.newTask = ""
+
+  // }
+  ngOnInit() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+      this.isAvalible = this.tasks.length > 0;
+    }
+  }
+
   addTask() {
     if (this.newTask.trim() !== "") {
-      this.tasks.push(this.newTask)
+      this.tasks.push(this.newTask);
       this.isAvalible = true;
-      this.newTask = ""
-      console.log(this.tasks)
+      this.newTask = "";
+      localStorage.setItem('tasks', JSON.stringify(this.tasks)); // Save to local storage
+      console.log(this.tasks);
     }
   }
+
+  EditTask(index: number, newTask: string) {
+    if (newTask.trim() !== "") {
+      this.tasks[index] = newTask;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks)); // Update local storage
+    }
+  }
+
   RemoveTask(index: number) {
     this.tasks.splice(index, 1);
+    localStorage.setItem('tasks', JSON.stringify(this.tasks)); // Update local storage
     this.isAvalible = this.tasks.length > 0;
-
-  }
-  EditTask(index: number, editnewtask: string): string | void {
-    if (editnewtask.trim() !== "") {
-      this.tasks[index] = editnewtask;
-
-    }
-    else {
-      editnewtask = this.tasks[index];
-      return this.newTask = editnewtask
-    }
-    this.newTask = ""
-
   }
 }
